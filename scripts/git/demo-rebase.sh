fc2875c12bbc24544ecfbd2bfa8bc48b44d60138
Jochen <jochen.handels@web.de>
2019-01-22 18:02:58 +0100

    Using redirection of stderr to indent function

--- scripts/git/demo-rebase.sh
+++ scripts/git/demo-rebase.sh
@@ -9,70 +9,70 @@ echo "Initialize"
 TMPDEMODIR=$(mktemp -d)
 pushd $TMPDEMODIR > /dev/null
 
-git init | indent
+git init |& indent
 
 echo "First commit on master"
 
 echo "A" > demo.txt
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Initial commit" | indent
+git commit -m "Initial commit" |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Second commit on master"
 
 echo "B" > demo.txt
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Changed first line" | indent
+git commit -m "Changed first line" |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Create feature branch"
 
-git branch feature01 | indent
+git branch feature01 |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Third commit on master"
 
 echo "C" > demo.txt
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Changed first line" | indent
+git commit -m "Changed first line" |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Commit on feature branch"
 
-git checkout feature01 | indent
+git checkout feature01 |& indent
 
 echo "A2" > demo2.txt
 
-git add demo2.txt | indent
+git add demo2.txt |& indent
 
-git commit -m "Changed second line" | indent
+git commit -m "Changed second line" |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Rebase feature branch to tip of master"
-git rebase master | indent
+git rebase master |& indent
 
 echo "Merge feature branch into master"
-git checkout master | indent
-git merge feature01 --ff-only | indent
+git checkout master |& indent
+git merge feature01 --ff-only |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Cleanup"
 popd > /dev/null
--- scripts/git/demo-reset.sh
+++ scripts/git/demo-reset.sh
@@ -9,91 +9,91 @@ echo "Initialize"
 TMPDEMODIR=$(mktemp -d)
 pushd $TMPDEMODIR > /dev/null
 
-git init | indent
+git init |& indent
 
 echo "First commit"
 
 echo "A
 B" > demo.txt
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Initial commit" | indent
+git commit -m "Initial commit" |& indent
 
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Second commit (1st)"
 
 echo "A
 B2" > demo.txt
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Changed second line" | indent
+git commit -m "Changed second line" |& indent
 
 echo "Git graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Soft reset"
 
-git reset HEAD~1 --soft | indent # moves only HEAD to 1st parent
+git reset HEAD~1 --soft |& indent # moves only HEAD to 1st parent
 
 echo "Graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Status:"
-git status --short | indent
+git status --short |& indent
 
 echo "Reset staged files"
 
-git reset | indent
+git reset |& indent
 
 echo "Second commit (2nd attempt)"
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Changed second line" | indent
+git commit -m "Changed second line" |& indent
 
 echo "Graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Mixed reset"
 
-git reset HEAD~1 | indent # moves HEAD to 1st parent and resets the staged files in the index. --mixed is the implicit default
+git reset HEAD~1 |& indent # moves HEAD to 1st parent and resets the staged files in the index. --mixed is the implicit default
 
 echo "Graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Second commit (3rd attempt)"
 
-git add demo.txt | indent
+git add demo.txt |& indent
 
-git commit -m "Changed second line" | indent
+git commit -m "Changed second line" |& indent
 
 echo "Graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Hard reset"
 
-git reset HEAD~1 --hard | indent # moves HEAD to 1st parent and resets the staged files in the index as well as the working directory
+git reset HEAD~1 --hard |& indent # moves HEAD to 1st parent and resets the staged files in the index as well as the working directory
 
 echo "Graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Status:"
-git status --short | indent
+git status --short |& indent
 
 echo "Reflog:"
-git reflog | indent
+git reflog |& indent
 
 echo "Undo reset:"
-git reset 'HEAD@{1}' --hard | indent
+git reset 'HEAD@{1}' --hard |& indent
 
 echo "Graph:"
-{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } | indent
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
 
 echo "Status:"
-git status --short | indent
+git status --short |& indent
 
 echo "Cleanup"
 popd > /dev/null
