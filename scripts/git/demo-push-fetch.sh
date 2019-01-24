52a514f978ba566feae70f2fcaaf882c889309c1
Jochen <jochen.handels@web.de>
2019-01-24 01:48:47 +0100

    Added demo for git push and fetch

---
+++ scripts/git/demo-push-fetch.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+LOGFORMAT="%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+
+indent() { sed 's/^/  /'; }
+
+echo "Initialize"
+
+TMPDEMODIR1=$(mktemp -d)
+TMPDEMODIR2=$(mktemp -d)
+pushd $TMPDEMODIR1 > /dev/null
+
+git init |& indent
+echo "* text eol=lf" > ".gitattributes"
+
+echo "Current: $PWD"
+
+echo "Commit on master"
+
+echo "A" > demo.txt
+
+git add demo.txt |& indent
+
+git commit -m "Initial commit" |& indent
+
+echo "Git graph:"
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
+
+cd $TMPDEMODIR2
+echo "Current: $PWD"
+
+echo "Create feature branch"
+
+git clone $TMPDEMODIR1 .
+
+echo "Create feature branch"
+
+git checkout -b feature01 |& indent
+
+echo "Git graph:"
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
+
+echo "Commit on feature branch"
+
+git checkout feature01 |& indent
+
+echo "B" > demo2.txt
+
+git add demo2.txt |& indent
+
+git commit -m "Added second file" |& indent
+
+echo "Git graph:"
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
+
+echo "Push to remote:"
+git push origin feature01 |& indent
+
+cd $TMPDEMODIR1
+echo "Current: $PWD"
+
+echo "Git graph:"
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
+
+echo "Merge feature branch into master"
+git merge feature01 |& indent
+
+cd $TMPDEMODIR2
+echo "Current: $PWD"
+
+echo "Fetch from remote"
+git fetch origin |& indent
+
+echo "Git graph:"
+{ git log --graph --full-history --all --color --pretty=format:$LOGFORMAT; echo; } |& indent
+
+echo "Cleanup"
+popd > /dev/null
+rm -rf $TMPDEMODIR1
+rm -rf $TMPDEMODIR2
